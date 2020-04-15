6cd8ee32330ccda96b8bb56b751104efb9cb14cb
Mike Dmitriev <misha.dmitriev.spb@gmail.com>
2020-04-15 20:17:09 +0300

    20_Near-Me

---
+++ 20_Near-Me/NearMeApp/Views/PlaceListView.swift
@@ -0,0 +1,42 @@
+//
+//  PlaceListView.swift
+//  NearMeApp
+//
+//  Created by Mohammad Azam on 3/25/20.
+//  Copyright © 2020 Mohammad Azam. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct PlaceListView: View {
+    
+    let landmarks: [Landmark]
+    var onTap: () -> ()
+    
+    var body: some View {
+        
+        VStack {
+            
+            HStack {
+                EmptyView()
+            }.frame(width: UIScreen.main.bounds.size.width, height: 60)
+                .background(Color.blue)
+                .gesture(TapGesture()
+                    .onEnded(self.onTap)
+            )
+            
+            List(self.landmarks, id: \.id) { landmark in
+                Text(landmark.name)
+            }.animation(nil)
+            
+        }.cornerRadius(16)
+        
+    }
+}
+
+struct PlaceListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaceListView(landmarks: [Landmark(placemark: MKPlacemark())], onTap: { })
+    }
+}
